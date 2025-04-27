b69ba86361e8d97f2eeb46235a8934223ea3d778
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2025-04-27 14:18:16 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from supabase import create_client, Client
+import uuid
+
+def main():
+    st.set_page_config(page_title="Sticker App", page_icon="🖼️", layout="centered")
+    st.header("Sticker Goat Gotta catch em all 🖼️🥳")
+    st.write("Du kannst Sticker entweder fotografieren oder aus deiner Galerie hochladen")
+    # Kamera
+    st.subheader("📷 Aktivie die Kamera")
+    kamera_aktiv = st.toggle("📷 Kamera aktivieren")
+    camera_image = None
+    file_name = None  # Initialize file_name
+    file_bytes = None  # Initialize file_bytes
+    if kamera_aktiv:
+        camera_image = st.camera_input("Fotografie den Sticker")
+    # File Upload
+    st.subheader("📤 Bild hochladen")
+    uploaded_file = st.file_uploader("Wähle ein Bild aus", type=["jpg", "png", "jpeg"])
+    if camera_image is not None:
+        st.image(camera_image, caption="Fotografiertes Bild", use_column_width=True)
+        file_bytes = camera_image.getvalue()  # Convert to bytes for upload
+        file_name = f"{uuid.uuid4()}.png"  # Unique file name for the image
+    elif uploaded_file is not None:
+        st.image(uploaded_file, caption="Hochgeladenes Bild", use_column_width=True)
+        file_bytes = uploaded_file.read()  # Convert to bytes for upload
+        file_name = f"{uuid.uuid4()}.png"  # Unique file name for the image
+        st.write(file_name)
+    # If neither camera nor file upload is provided, exit early
+    if file_name is None or file_bytes is None:
+        st.error("❌ Kein Bild zum Hochladen ausgewählt.")
+        return
+    # Datenbank Verbindung
+    if st.button("Eingaben bestätigen"):
+        try:
+            supabase_url = st.secrets["credentials"]["SUPABASE_URL"]
+            supabase_key = st.secrets["credentials"]["SUPABASE_KEY"]
+            supabase_client: Client = create_client(supabase_url, supabase_key)
+            st.success("✅ Verbindung mit der Datenbank erfolgreich hergestellt!")
+            bucket_name = "stickergoat"
+            file_path = f"{bucket_name}/{file_name}"
+            response = supabase_client.storage.from_(bucket_name).upload(file_path, file_bytes)
+        except Exception as e:
+            st.error(f"❌ Es kann keine Verbindung zur Datenbank hergestellt werden: {e}")
+
+if __name__ == "__main__":
+    main()
